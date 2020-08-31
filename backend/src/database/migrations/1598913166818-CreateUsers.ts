3a78b5a5bd4d1905865faf5f51369e1ecbf9e0a0
Cristian Butzke <cristian.btzk@gmail.com>
2020-08-31 20:58:14 -0300

    Backend users migration, model created

--- backend/src/database/migrations/1598913166818-CreateUsers.ts
+++ backend/src/database/migrations/1598913166818-CreateUsers.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import { formatDistanceToNow } from 'date-fns';
 
-export default class CreateAppointments1598907365643
-  implements MigrationInterface {
+export default class CreateUsers1598913166818 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,14 +15,18 @@ export default class CreateAppointments1598907365643
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
             isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
-            isNullable: false,
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -40,6 +44,6 @@ export default class CreateAppointments1598907365643
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
