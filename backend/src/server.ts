6dd48ad62a038cb68b14038b0253664d7ac8ca8e
Cristian Butzke <cristian.btzk@gmail.com>
2020-09-01 17:08:03 -0300

    Backend avatar upload

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,11 @@ import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
 import './database';
+import uploadConfig from './config/upload';
 
 const server = express();
 server.use(express.json());
+server.use('/files', express.static(uploadConfig.directory));
 server.use(routes);
 
 server.listen(3333);
