b281ea61ecb23bd4da0393df88efc1db344b2208
Cristian Butzke <cristian.btzk@gmail.com>
2021-01-07 21:22:23 -0300

    update profile, avatar

--- backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -42,6 +42,9 @@ class AppointmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      order: {
+        date: 'ASC',
+      },
 
       relations: ['user'],
     });
